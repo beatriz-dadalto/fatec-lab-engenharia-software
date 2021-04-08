d37310fec5935766389db2573a848229ae4a4313
Bia Coelho <biacoelho@pm.me>
2021-04-08 03:41:02 -0300

    add: aula6 part1

---
+++ src/test/java/edu/curso/medicos/MedicosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.curso.medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
